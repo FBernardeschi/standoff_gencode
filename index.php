c2073e698a39805a08b6aa1d1d70e61ce244cc6c
Bernardeschi <kkostik100@yandex.ru>
2023-05-27 20:13:44 +0500

    fix bag

--- index.php
+++ index.php
@@ -6,6 +6,31 @@ require_once 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
 
+$randomPayment = rand(100, 100000);
+
+$params = array(
+    $amount = 10, // цена
+    $payment = $randomPayment, // ид номера заказа (не трогай!)
+    $shop = 999, // id магазина
+    $currency = 'RUB',
+    $desc = 'StandOFF 2',
+    $secret = '' // секретный код
+);
+
+$sign = md5( implode ( '|', $params ) );
+
+$paramsUrl = array(
+    'amount' => $params[0],
+    'payment' => $params[1],
+    'shop' => $params[2],
+    'currency' => $params[3],
+    'desc' => $params[4],
+    'sign' => $sign
+);
+
+$url = 'https://payok.io/pay?' . http_build_query($paramsUrl);
+
 echo $twig->render('index.html', [
-    
+    'url'=>$url,
+    'price'=>$params[0]
 ]);
--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function randCode() {
 
 let login = document.querySelectorAll('.loginA');
 let reg = document.querySelectorAll('.regA');
-let gen = document.querySelector('.genA');
+let gen = document.querySelectorAll('.genA');
 let mainList = document.querySelector('.main-list');
 
 
@@ -37,9 +37,11 @@ reg.forEach((el) => {
     })
 })
 
-gen.addEventListener('click', function() {
-    mainList.classList.remove('active-reg');
-    mainList.classList.remove('active-login');
+gen.forEach((el) => {
+    el.addEventListener('click', function() {
+        mainList.classList.remove('active-reg');
+        mainList.classList.remove('active-login');
+    })    
 })
 
 // menu toggler
@@ -49,4 +51,41 @@ let nav = document.querySelector('.nav');
 
 iconToggler.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+})
+
+// login
+
+let emailLogin = document.querySelector('.login input[type=text]');
+let passLogin = document.querySelector('.login input[type=password]');
+let msgErrorLogin = document.querySelector('.login .block-input>p');
+let btnLogin = document.querySelector('.login input[type=submit]');
+
+function msgErrorEnter() {
+    msgErrorLogin.innerHTML = 'Логин или пароль введены неверно!'
+};
+
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(emailLogin.value && passLogin.value) {
+        setTimeout(msgErrorEnter, 1500);
+    } else {
+        msgErrorLogin.innerHTML = 'Введены не все поля!';;
+    }
+})
+
+// reg
+
+let emailReg = document.querySelector('.reg input[type=text]');
+let passReg = document.querySelector('.reg input[type=password]');
+let passReg2 = document.querySelector('.reg .input-box:nth-child(3) input[type=password]');
+let msgErrorReg = document.querySelector('.reg .block-input>p');
+let btnReg = document.querySelector('.reg a.form-btn');
+
+btnReg.addEventListener('click', (e) => {
+    if(emailReg.value && passReg.value && passReg2.value) {
+        btnReg.innerHTML = 'Ожидается оплата!';
+    } else {    
+        e.preventDefault()
+        msgErrorReg.innerHTML = 'Введены не все поля!';;
+    }
 })
