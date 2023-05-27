2982068686d9016624081d9015d0c8f8ec81b45b
Bernardeschi <kkostik100@yandex.ru>
2023-05-27 14:56:09 +0500

    add adaptiv

--- js/app.js
+++ js/app.js
@@ -17,22 +17,36 @@ function randCode() {
 
 // scroll
 
-let login = document.querySelector('.loginA');
-let reg = document.querySelector('.regA');
+let login = document.querySelectorAll('.loginA');
+let reg = document.querySelectorAll('.regA');
 let gen = document.querySelector('.genA');
 let mainList = document.querySelector('.main-list');
 
-login.addEventListener('click', function() {
-    mainList.classList.add('active-login');
-    mainList.classList.remove('active-reg');
+
+login.forEach((el) => {
+    el.addEventListener('click', function() {
+        mainList.classList.add('active-login');
+        mainList.classList.remove('active-reg');
+    });
 });
 
-reg.addEventListener('click', function() {
-    mainList.classList.add('active-reg');
-    mainList.classList.remove('active-login');
+reg.forEach((el) => {
+    el.addEventListener('click', function() {
+        mainList.classList.add('active-reg');
+        mainList.classList.remove('active-login');
+    })
 })
 
 gen.addEventListener('click', function() {
     mainList.classList.remove('active-reg');
     mainList.classList.remove('active-login');
+})
+
+// menu toggler
+
+let iconToggler = document.querySelector('.icon-toggler');
+let nav = document.querySelector('.nav');
+
+iconToggler.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
 })
